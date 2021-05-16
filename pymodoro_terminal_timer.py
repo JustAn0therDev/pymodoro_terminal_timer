c5c9d6d1c720dcab1c42a0ab83aee774fc2e89e3
JustAn0therDev <ruanmontelo@gmail.com>
2021-05-16 19:30:45 -0300

    Adding debug option in the main script

--- pymodoro_terminal_timer.py
+++ pymodoro_terminal_timer.py
@@ -6,18 +6,19 @@ from datetime import datetime, timedelta
 option_descriptions = [
     '1 - Start work: twenty five minutes', 
     '2 - Start quick break: five minutes', 
-    '3 - Start long break: fifteen minutes'
+    '3 - Start long break: fifteen minutes',
+    '4 - Debug program behaviour: six seconds'
 ]
-
-def get_time_diff_in_minutes(minutes: int):
-    return datetime.now() + timedelta(minutes=minutes)
 
 time_options_in_minutes = {
     1: 25,
     2: 5,
-    3: 15
-    # 4: 0.10 - DEBUGGER -> Roughly 6 seconds
+    3: 15,
+    4: 0.10
 }
+
+def get_time_diff_in_minutes(minutes: int):
+    return datetime.now() + timedelta(minutes=minutes)
 
 while(True):
         
@@ -25,7 +26,9 @@ while(True):
         
     chosen_option = int(input('Choose an option: '))
 
-    if chosen_option not in range(1, len(option_descriptions) + 1):
+    description_limit = len(option_descriptions) + 1
+
+    if chosen_option not in range(1, description_limit):
        print('The selected option is invalid. Please select another option: ')
        continue
 
@@ -34,4 +37,3 @@ while(True):
     datetime_with_added_minutes_from_now = get_time_diff_in_minutes(chosen_minutes)
 
     print_timer(datetime_with_added_minutes_from_now)
-
